458e0d7cab19daa364ba94566545c20844af5f81
Patrick <patrick@sendhelp.biz>
2022-03-15 18:34:49 -0700

    form test

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -3,26 +3,22 @@ import * as React from "react"
 const RegistrationPage = () => {
   return (
 
-<form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+<>
+    <h1>Contact</h1>
+
+    <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <label>Your Email:</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+      </div>
+      <button type="submit">Send</button>
+    </form>
+</>
 
   )
 }
