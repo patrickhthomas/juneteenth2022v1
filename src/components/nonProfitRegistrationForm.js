4403572d0c48b34aca5e28978cf9e8ba946f2018
Patrick <patrick@sendhelp.biz>
2022-03-18 12:44:42 -0700

    updates form-name

--- src/components/nonProfitRegistrationForm.js
+++ src/components/nonProfitRegistrationForm.js
@@ -5,28 +5,7 @@ import { navigate } from "gatsby"
 import * as styles from "../components/nonProfitRegistrationForm.module.scss"
 
 
-function encode(data) {
-  return Object.keys(data)
-    .map(
-      (key) =>
-        encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-    )
-    .join("&");
-}
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({
-      "form-name": event.target.getAttribute("name"),
-      ...name,
-    }),
-  })
-    .then(() => navigate("/thank-you/"))
-    .catch((error) => alert(error));
-};
 
 const NonProfitRegstrationForm = () => {
     return (
